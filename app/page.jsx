deda12846996f8625dc36a4781c5c682e1c87b70
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-30 18:37:57 +0530

    Add prettier-tailwind plugin & update layout.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1 className="text-4xl font-bold tracking-tighter">
+        hello world
+      </h1>
     </div>
   );
 }
