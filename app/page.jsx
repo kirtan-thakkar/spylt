f432a0e6b50fdee7c5d0a39c59be078f46d7c8e2
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-30 19:53:38 +0530

    Add gsap & react-responsive

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold tracking-tighter">
-        hello world
-      </h1>
+      <h1 className="text-4xl font-bold tracking-tighter">hello world</h1>
     </div>
   );
 }
