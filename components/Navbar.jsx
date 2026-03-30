c84e09fabab09e2226a947ba5b075ef7a91671c8
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-30 21:26:48 +0530

    Complete navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 
-const Navbar=()=>{
-    return(
-        <div>
-            <h1 className="text-2xl font-bold tracking-tighter text-shadow-lg">Navbar</h1>
-        </div>
-    )
-}
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 p-3 md:p-9">
+      <Image
+        src="/images/nav-logo.svg"
+        alt="Logo"
+        width={100}
+        height={50}
+        className="w-20 md:w-24"
+      />
+    </nav>
+  );
+};
 export default Navbar;
