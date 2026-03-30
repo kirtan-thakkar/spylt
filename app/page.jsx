fae3ef7d1e0e751f2df95b1bf1ac9a1e53deab33
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-30 19:58:42 +0530

    Initialsed navbar.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold tracking-tighter">hello world</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+const Navbar=()=>{
+    return(
+        <div>
+            <h1 className="text-2xl font-bold tracking-tighter text-shadow-lg">Navbar</h1>
+        </div>
+    )
+}
+export default Navbar;
