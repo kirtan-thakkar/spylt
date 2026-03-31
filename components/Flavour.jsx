8bfad748d1d5d29e7ff6607ee33d83b8e19753b0
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-31 22:02:59 +0530

    Initialse Flavour.jsx

---
+++ components/Flavour.jsx
@@ -0,0 +1,13 @@
+const FlavourPage = () => {
+  return (
+    <section className="flavor-section">
+      {/* wrapper div */}
+      <div className="relative h-full flex-col items-center lg:flex-row">
+        <div className="h-80 flex-none md:mt-20 lg:h-full lg:w-[57%] xl:mt-0">
+            
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
