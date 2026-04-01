c4d7daac899196cf871a460bb5445a3d6519f626
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-01 22:42:48 +0530

    Add horizontal scrolling animation

--- components/Flavour.jsx
+++ components/Flavour.jsx
@@ -1,8 +1,33 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import FlavourSlider from "./FlavourSlider";
 import FlavourTitle from "./FlavourTitle";
+import { useRef } from "react";
 const FlavourPage = () => {
+  const sliderRef = useRef(null);
+  useGSAP(()=>{
+    const section = sliderRef.current;
+    if (!section || typeof window === "undefined") return;
+
+    const scrollAmount = section.scrollWidth - window.innerWidth;
+
+    const t1 = gsap.timeline({
+      scrollTrigger:{
+        trigger:".flavor-section",
+        start:"2% top",
+        end:`+=${scrollAmount}px`,
+        pin:true,
+        scrub:true,
+      }
+    })
+    t1.to(".flavor-section",{
+      x:`-${scrollAmount}px`,
+      ease:"power1.inOut",
+    })
+
+  },[])
   return (
-    <section className="flavor-section">
+    <section ref={sliderRef} className="flavor-section">
       {/* wrapper div */}
       <div className="relative flex h-full flex-col items-center lg:flex-row">
         <div className="h-80 flex-none md:mt-20 lg:h-full lg:w-[57%] xl:mt-0">
