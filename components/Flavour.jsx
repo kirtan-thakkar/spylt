8d341d943652b3016f139158b32687ce068a0da2
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-01 22:18:03 +0530

    Add images in FlavourSlider.jsx

--- components/Flavour.jsx
+++ components/Flavour.jsx
@@ -1,15 +1,15 @@
+import FlavourSlider from "./FlavourSlider";
 import FlavourTitle from "./FlavourTitle";
-
 const FlavourPage = () => {
   return (
     <section className="flavor-section">
       {/* wrapper div */}
       <div className="relative h-full flex-col items-center lg:flex-row">
         <div className="h-80 flex-none md:mt-20 lg:h-full lg:w-[57%] xl:mt-0">
-            <FlavourTitle />
+          <FlavourTitle />
         </div>
-        <div className="h-full">
-
+        <div className="relative z-50 h-full">
+          <FlavourSlider />
         </div>
       </div>
     </section>
--- components/FlavourSlider.jsx
+++ components/FlavourSlider.jsx
@@ -1,8 +1,28 @@
-const FlavourSlider = ()=>{
-    return(
-        <div>
-            Flavor Slider
-        </div>
-    )
-}
+import { flavorlists } from "@/app/constants";
+
+const FlavourSlider = () => {
+  return (
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor, index) => (
+          <div
+            key={index}
+            className="relative z-30 h-80 w-96 flex-none md:h-[50vh] md:w-[90vw] lg:h-[70vh] lg:w-[50vw]"
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt={flavor.name}
+              className="absolute bottom-0"
+            />
+            <img
+              src={`/images/${flavor.color}-drink.webp `}
+              alt={flavor.name}
+              className="absolute bottom-0"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default FlavourSlider;
