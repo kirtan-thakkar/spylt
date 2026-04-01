20906372be7731b4faf912d31ce3304f85788714
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-01 22:29:01 +0530

    Add proper styling to imgs -> flavourSlider.jsx

--- components/Flavour.jsx
+++ components/Flavour.jsx
@@ -4,11 +4,11 @@ const FlavourPage = () => {
   return (
     <section className="flavor-section">
       {/* wrapper div */}
-      <div className="relative h-full flex-col items-center lg:flex-row">
+      <div className="relative flex h-full flex-col items-center lg:flex-row">
         <div className="h-80 flex-none md:mt-20 lg:h-full lg:w-[57%] xl:mt-0">
           <FlavourTitle />
         </div>
-        <div className="relative z-50 h-full">
+        <div className="h-full">
           <FlavourSlider />
         </div>
       </div>
--- components/FlavourSlider.jsx
+++ components/FlavourSlider.jsx
@@ -7,7 +7,7 @@ const FlavourSlider = () => {
         {flavorlists.map((flavor, index) => (
           <div
             key={index}
-            className="relative z-30 h-80 w-96 flex-none md:h-[50vh] md:w-[90vw] lg:h-[70vh] lg:w-[50vw]"
+            className={`relative z-30 h-80 w-96 flex-none md:h-[50vh] md:w-[90vw] lg:h-[70vh] lg:w-[50vw] ${flavor.rotation}`}
           >
             <img
               src={`/images/${flavor.color}-bg.svg`}
@@ -19,6 +19,14 @@ const FlavourSlider = () => {
               alt={flavor.name}
               className="drinks"
             />
+            <img
+              src={`/images/${flavor.color}-elements.webp `}
+              alt={flavor.name}
+              className="elements"
+            />
+
+            <h1>{flavor.name}</h1>
+            
           </div>
         ))}
       </div>
