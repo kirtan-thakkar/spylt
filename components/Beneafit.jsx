74bb8ddcbac82c2b8ae4c7fa547b6b7bdb3c9c36
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-03 23:31:14 +0530

    Complete beneafit.jsx

--- components/Beneafit.jsx
+++ components/Beneafit.jsx
@@ -1,6 +1,44 @@
+import { useGSAP } from "@gsap/react";
 import ClipPathTitle from "./ClipPathTitle";
-
+import gsap from "gsap";
 const BeneafitPage = () => {
+  useGSAP(() => {
+    const revealTimeline = gsap.timeline({
+      delay: 2,
+      scrollTrigger: {
+        trigger: ".benefit-section",
+        start: "top 60%",
+        end: "top top",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+    revealTimeline
+      .to(".benefit-section .first-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .second-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .third-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      })
+      .to(".benefit-section .fourth-title", {
+        duration: 1,
+        opacity: 1,
+        clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+        ease: "circ.out",
+      });
+  }, []);
   return (
     <section className="benefit-section">
       <div className="container mx-auto pt-20">
@@ -9,15 +47,41 @@ const BeneafitPage = () => {
             Unlock the Advantages: <br />
             Explore the Key Benefits of Choosing SPYLT
           </p>
-          <div className="mt-20 col-center">
-            <ClipPathTitle title={"Shelf stable"} color={"#faeade"} bg={"#c88e64"} className={"first-title"} borderColor={"#222123"} />
+          <div className="col-center mt-20">
+            <ClipPathTitle
+              title={"Shelf stable"}
+              color={"#faeade"}
+              bg={"#c88e64"}
+              className={"first-title"}
+              borderColor={"#222123"}
+            />
 
-            <ClipPathTitle title={"Shelf stable"} color={"#faeade"} bg={"#c88e64"} className={"first-title"} borderColor={"#222123"} />
-            
-            <ClipPathTitle title={"Shelf stable"} color={"#faeade"} bg={"#c88e64"} className={"first-title"} borderColor={"#222123"} />
-            
-            <ClipPathTitle title={"Shelf stable"} color={"#faeade"} bg={"#c88e64"} className={"first-title"} borderColor={"#222123"} />
+            <ClipPathTitle
+              title={"Protein + Caffeine"}
+              color={"#222123"}
+              bg={"#faeade"}
+              className={"second-title"}
+              borderColor={"#222123"}
+            />
 
+            <ClipPathTitle
+              title={"Infinitely recyclable"}
+              color={"#faeade"}
+              bg={"#7F3B2D"}
+              className={"third-title"}
+              borderColor={"#222123"}
+            />
+
+            <ClipPathTitle
+              title={"Lactose free"}
+              color={"#2E2D2F"}
+              bg={"#FED775"}
+              className={"fourth-title"}
+              borderColor={"#222123"}
+            />
+          </div>
+          <div className="mt-10 md:mt-0">
+            <p>ANd much more ...</p>
           </div>
         </div>
       </div>
--- components/ClipPathTitle.jsx
+++ components/ClipPathTitle.jsx
@@ -3,11 +3,11 @@ const ClipPathTitle = ({ title, color, bg, className, borderColor }) => {
     <div className="general-title">
       <div
         style={{
-          clipPath: "",
+          clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%);",
           borderColor: borderColor,
           color: color,
         }}
-        className={`${className} border-[1.5vw] text-nowrap opacity-100`}
+        className={`${className} border-[1.5vw] text-nowrap opacity-0`}
       >
         <div
           className="px-3 pt-3 pb-5 md:px-14 md:pt-0"
