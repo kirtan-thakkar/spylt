c58f1323372b0a6c1c197c89ae0dee6cdbb999ce
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-05 00:36:17 +0530

    complete Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,37 @@
+import { useMediaQuery } from "react-responsive";
 const FooterPage = () => {
+    const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
   return (
     <section className="footer-section">
       <img
         src="/images/footer-dip.png"
-        alt="footer-dip"
-        className="size-full -translate-y-1 object-cover"
+        alt=""
+        className="w-full object-cover -translate-y-1"
       />
-      <div className="relative pt-[10vh] md:pt-[20vh] 2xl:h-[110dvh]">
-        <div className="z-10 overflow-hidden">
-          <h1 className="general-title text-milk py-5 text-center">
+
+      <div className="2xl:h-[110dvh] relative md:pt-[20vh] pt-[10vh]">
+        <div className="overflow-hidden z-10">
+          <h1 className="general-title text-center text-milk py-5">
             #CHUGRESPONSIBLY
           </h1>
         </div>
 
-        <video
-          src="/videos/splash.mp4"
-          playsInline
-          autoPlay
-          muted
-          className="absolute top-0 object-contain mix-blend-lighten"
-        />
+        {isMobile ? (
+          <img
+            src="/images/footer-drink.png"
+            className="absolute top-0 object-contain"
+          />
+        ) : (
+          <video
+            src="/videos/splash.mp4"
+            autoPlay
+            playsInline
+            muted
+            className="absolute top-0 object-contain mix-blend-lighten"
+          />
+        )}
         <div className="flex-center relative z-10 mt-5 gap-5 md:mt-20">
           <div className="social-btn">
             <img src="/images/yt.svg" alt="YouTube" />
@@ -30,7 +42,51 @@ const FooterPage = () => {
           <div className="social-btn">
             <img src="/images/yt.svg" alt="YouTube" />
           </div>
+        </div>
 
+        <div className="text-milk font-paragraph mt-40 flex flex-col justify-between gap-10 px-5 font-medium md:flex-row md:px-10 md:text-lg">
+          <div className="flex items-center gap-5 md:gap-16">
+            <div>
+              <p>SPYLT Flavors</p>
+            </div>
+            <div>
+              <p>Chug Club</p>
+              <p>Student Marketing</p>
+              <p>Dairy Dealers</p>
+            </div>
+            <div>
+              <p>Company</p>
+              <p>Contacts</p>
+              <p>Tasty Talk</p>
+            </div>
+          </div>
+
+          <div className="md:max-w-lg">
+            <p>
+              Get Exclusive Early Access and Stay Informed About Product
+              Updates, Events, and More!
+            </p>
+            <div className="flex items-center justify-between border-b border-[#D9D9D9] py-5 md:mt-10">
+              {/* The input field and arrow icon for newsletter signup. */}{" "}
+              {/* A
+          border at the bottom for a clean, modern look. */}
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full placeholder:font-sans placeholder:text-[#999999]"
+              />
+              <img src="/images/arrow.svg" alt="arrow" />
+            </div>
+          </div>
+        </div>
+
+        <div className="copyright-box">
+          {/* The final row with copyright and legal links. */}
+          <p>Copyright © 2026 Kirtan Thakkar - All Rights Reserved</p>
+          <div className="flex items-center gap-7">
+            <p>Privacy Policy</p>
+            <p>Terms of Sеrvice</p>
+          </div>
         </div>
       </div>
     </section>
