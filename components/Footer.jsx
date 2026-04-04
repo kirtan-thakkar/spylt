7a58e38d8585e502821de6160907968dee0556f6
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-05 00:30:39 +0530

    Add video and social links -> Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,39 @@
-const FooterPage = ()=>{
-    return(
-        <div>
-            <h1>Footer Section</h1>
+const FooterPage = () => {
+  return (
+    <section className="footer-section">
+      <img
+        src="/images/footer-dip.png"
+        alt="footer-dip"
+        className="size-full -translate-y-1 object-cover"
+      />
+      <div className="relative pt-[10vh] md:pt-[20vh] 2xl:h-[110dvh]">
+        <div className="z-10 overflow-hidden">
+          <h1 className="general-title text-milk py-5 text-center">
+            #CHUGRESPONSIBLY
+          </h1>
         </div>
-    )
-}
+
+        <video
+          src="/videos/splash.mp4"
+          playsInline
+          autoPlay
+          muted
+          className="absolute top-0 object-contain mix-blend-lighten"
+        />
+        <div className="flex-center relative z-10 mt-5 gap-5 md:mt-20">
+          <div className="social-btn">
+            <img src="/images/yt.svg" alt="YouTube" />
+          </div>
+          <div className="social-btn">
+            <img src="/images/insta.svg" alt="Instagram" />
+          </div>
+          <div className="social-btn">
+            <img src="/images/yt.svg" alt="YouTube" />
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
 export default FooterPage;
