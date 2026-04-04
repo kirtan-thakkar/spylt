f014068beb74bab1cff2aa5b54216084945006ba
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-04 22:37:20 +0530

    Remove the animation for mobile devices

--- components/VideoPin.jsx
+++ components/VideoPin.jsx
@@ -1,10 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { Video } from "lucide-react";
+import { useMediaQuery } from "react-responsive";
 
 const VideoPin = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   useGSAP(() => {
-    const t1 = gsap.timeline({
+    if(!isMobile){
+      const t1 = gsap.timeline({
       scrollTrigger: {
         trigger: ".vd-pin-section",
         start: "-15% top", //the animation will start when the top of the section is 15% above the top of the viewport
@@ -13,18 +16,19 @@ const VideoPin = () => {
         markers: true,
         pin: true,
       },
-    });
-    t1.to(".vd-pin-section .video-box", {
+      });
+      t1.to(".vd-pin-section .video-box", {
       duration: 1,
       clipPath: "circle(70.7% at 50% 50%)",
       ease: "power1.inOut",
     });
+    }
   });
   return (
     <section className="vd-pin-section">
       <div
         style={{
-          clipPath: "circle(13.6% at 50% 50%)",
+          clipPath: isMobile ? "circle(70.7% at 50% 50%)" : "circle(13.6% at 50% 50%)",
         }}
         className="video-box size-full"
       >
