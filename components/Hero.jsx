4024a107dc18099fdf4724240291ade0f429e87f
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-04-05 13:04:35 +0530

    Revert "Add Hero video & images for tablet and phone"
    This reverts commit f5a65c9873b73d2bfe62045758e25569ee64c4cf.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,11 +2,9 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import { useMediaQuery } from "react-responsive";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Hero = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" }); 
-  const isTablet = useMediaQuery({ query: "(max-width: 1023px)" });
   useGSAP(() => {
     const split = new SplitText(".hero-title", { type: "chars" });
     const timeline = gsap.timeline({
@@ -57,16 +55,11 @@ const Hero = () => {
     <>
       <section className="bg-main-bg">
         <div className="hero-container">
-          {
-            isTablet ? (
-              <>
-                {isMobile &&  <img src="/images/hero-bg.png" className="absolute bottom-40 size-full object-cover" />}
-                <img src="/images/hero-img.png" className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto "/>
-              </> 
-            ) : (
-              <video src="/videos/hero-bg.mp4" autoPlay playsInline muted  className="absolute inset-0 size-full object-cover"/>
-            ) 
-          }
+          <img
+            src="/images/static-img.png"
+            alt="Hero Image"
+            className="object-auto absolute bottom-0 left-1/2 -translate-x-1/2 scale-100 md:scale-150"
+          />
           <div className="hero-content opacity-0">
             {/* Going to animate hence overflow-hidden */}
             <div className="overflow-hidden">
